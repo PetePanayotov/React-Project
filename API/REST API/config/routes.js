2506a2987f4a16f65c680d749ffbe5961c3dcea6
PetePanayotov <ppanayotov97@gmail.com>
2020-07-22 18:54:47 +0300

    feat/ edited the user functionality of the REST API

--- API/REST API/config/routes.js
+++ API/REST API/config/routes.js
@@ -1,10 +1,10 @@
-const router = require('../routes/');
+const {userRouter , origami} = require('../routes/');
 
 module.exports = (app) => {
 
-    app.use('/api/user', router.user);
+    app.use('/api/user', userRouter);
 
-    app.use('/api/origami', router.origami);
+    app.use('/api/origami', origami);
 
     app.use('*', (req, res, next) => res.send('<h1> Something went wrong. Try again. :thumbsup: </h1>'))
 };
