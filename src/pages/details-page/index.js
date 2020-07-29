ff338e9dd5077c60fcc9b881c66ac41a920cd24f
PetePanayotov <ppanayotov97@gmail.com>
2020-07-29 16:07:24 +0300

    feat/details page

---
+++ src/pages/details-page/index.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react';
+import PageWrapper from '../../components/page-wrapper';
+import Main from '../../components/main';
+
+
+function DetailsPage() {
+
+    return(
+        <PageWrapper>
+            <Main layout="about">
+
+            </Main>
+        </PageWrapper>
+    );
+   
+};
+
+export default DetailsPage
--- src/pages/user-home-page/index.js
+++ src/pages/user-home-page/index.js
@@ -1,27 +1,21 @@
-import React , {Component} from 'react';
+import React from 'react';
 import PageWrapper from '../../components/page-wrapper';
 import Main from '../../components/main';
-import Car from '../../components/car';
 import Cars from '../../components/cars-container';
 import Title from '../../components/title';
 
 
-class UserHomePage extends Component {
-    constructor(props) {
-        super(props);
-    };
+function UserHomePage() {
 
-
-    render() {
-        return(
-            <PageWrapper>
-                <Main layout="about">
-                    <Title text="Our best offers"/>
-                    <Cars/>
-                </Main>
-            </PageWrapper>
-        );
-    };
+    return(
+        <PageWrapper>
+            <Main layout="about">
+                <Title text="Our best offers"/>
+                <Cars/>
+            </Main>
+        </PageWrapper>
+    );
+   
 };
 
 export default UserHomePage;
